dfa1651d943e4d4426d6249967cd868a85b793a5
luish-uk <luishernandezlg77@gmail.com>
2025-08-27 08:53:39 +0100

    Added better line spacing

--- main.js
+++ main.js
@@ -2,7 +2,7 @@
 with two lines */
 console.log("Hello, World!");
 
-const greeting = "Welcome to my js tip calculator!";
+const greeting = "Welcome to my js tip calculator!\n\n";
 console.log(greeting);
 
 const tips = [0, 'chewing gum', 3, 5, 7, 10, 15, 20, 25, 30, 50]
@@ -36,13 +36,13 @@ const millionaire_min = tips.reduce((tip, total_tip) => {
   return total_tip += tip
 }, 1000)
 
-console.log(`Just a quick headsup that it is ${too_little} that anything under 3% is too little and it is also ${egregious_amount} that anything above 50% is the same.. in Monaco.`)
+console.log(`Just a quick headsup that it is ${too_little} that anything under 3% is too little and it is also ${egregious_amount} that anything above 50% is the same.. in Monaco.\n`)
 
-console.log(`For restaurants you want to tip within this range: ${restaurant}%, it all depends on food quality`)
+console.log(`For restaurants you want to tip within this range: ${restaurant}%, it all depends on food quality\n`)
 
-console.log(`For hairdressers it doesn\'t matter weather they did a good job or not, you should tip in this range ${hairdresser}`)
+console.log(`For hairdressers it doesn\'t matter weather they did a good job or not, you should tip in this range ${hairdresser}\n`)
 
-console.log(`Pizza delivery people only deserve ${pizza_delivery} thing and that's ${tips[pizza_delivery]}`)
+console.log(`Pizza delivery people only deserve ${pizza_delivery} thing and that's ${tips[pizza_delivery]}\n`)
 
 //Always keep this at the end as well
-console.log(`And if you are a millionaire you are required by law to tip at least ${millionaire_min}% but any of these tips shown bellow are generous \n${millionaire}%`)
+console.log(`And if you are a millionaire you are required by law to tip at least ${millionaire_min}% but any of these tips shown bellow are generous \n${millionaire}%\n`)
